a94f420174680688b10606c7f1105a615b7a4775
Jiadai Liu <jliuapp@gmail.com>
2019-08-21 14:19:50 -0700

    Add files via upload

---
+++ src/components/Cart.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class Cart extends React.Component {
+
+	render() {
+		return (
+			<div>hello from cart</div>
+		);
+	}
+}
---
+++ src/components/Default.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class Default extends React.Component {
+
+	render() {
+		return (
+			<div>page not found</div>
+		);
+	}
+}
